db337bbdb2cd6d2c0a23a1ecac0a07673767bf4d
Nanda Velugoti <nvelugoti@hawk.iit.edu>
2021-11-11 17:10:00 -0600

    Add additional instructions

--- env_setup.sh
+++ env_setup.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
+
+if [ -z "$UPMEM_HOME" ] then
+    echo "Please install and setup UPMEM SDK. For more see: https://sdk.upmem.com/"
+    exit 1
+fi
+
 # export upem-sdk paths
-export PATH=$PATH:$HOME/upmem-sdk/bin
-export CPATH=$CPATH:$HOME/upmem-sdk/include/dpu
-export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$HOME/upmem-sdk/share/pkg-config
-export PKG_CONFIG_LIBDIR=$PKG_CONFIG_LIBDIR:$HOME/upmem-sdk/lib64
+export PATH=$PATH:$UPMEM_HOME/bin
+export CPATH=$CPATH:$UPMEM_HOME/include/dpu
+export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$UPMEM_HOME/share/pkg-config
+export PKG_CONFIG_LIBDIR=$PKG_CONFIG_LIBDIR:$UPMEM_HOME/lib64
